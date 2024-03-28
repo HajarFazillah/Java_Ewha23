51997b2d47d97d67f1df4ce581b67497d6caf44d
ASUS <ASUS@Hajar-Fazillah>
2024-03-29 00:48:11 +0900

    Initial Commit

---
+++ HelloJava/src/Hello2030.java
@@ -0,0 +1,15 @@
+
+public class Hello2030 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+		
+		double radius = 10.0;
+		double circleArea = radius*radius*PI;
+		
+		System.out.println("원의 면적 = " + circleArea); // 문자열 string 처럼 출력하게 됨
+		
+	}
+
+}
