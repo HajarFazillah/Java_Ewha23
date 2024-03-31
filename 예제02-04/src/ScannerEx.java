ba46f2147563096c451958047a8818f0785085ec
ASUS <ASUS@Hajar-Fazillah>
2024-03-31 23:38:29 +0900

    First Commit

---
+++ 예제02-04/src/ScannerEx.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ScannerEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("");
+		Scanner scanner = new Scanner(System.in);
+		/*
+		String name = scanner.next();
+		System.out.println("이름은 ");
+		
+		String city = scanner.next();
+		System.out.println("");
+		
+		int age = scanner.next();
+		System.out.println("");
+		
+		double weight
+		*/
+	}
+
+}
