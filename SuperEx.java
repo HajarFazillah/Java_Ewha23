6251166f94b12ff48130ef704d0cd77c416a69f1
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-04-07 18:29:09 +0900

    Add files via upload

--- SuperEx.java
+++ SuperEx.java
@@ -27,7 +27,6 @@ class ColorPoint extends Point{
 public class SuperEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ColorPoint cp = new ColorPoint(5,6,"blue");
 		cp.showColorPoint();
 	}
